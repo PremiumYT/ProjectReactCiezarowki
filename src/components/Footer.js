ba4dc95bbef329eb0db55f61a4962e1fa86a3392
PremiumYT <51030280+PremiumYT@users.noreply.github.com>
2025-11-29 16:35:25 +0000

    skonczenie footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import { Container } from "react-bootstrap";
 function Footer() {
     return (
         <footer>
-            <div className="fixed-bottom text-center bg-black text-white">
-                Footer
+            <div className="fixed-bottom text-center bg-dark text-white p-2">
+                Strona zrobiona przez Marcina Wirkowskiego
             </div>
         </footer>
     )
